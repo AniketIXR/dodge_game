85eb87e0e9eb24699fbaf7d17baa02f540b30f80
Aniket Roy <40049375+AniketIXR@users.noreply.github.com>
2022-02-18 10:28:01 +0530

    unityprojectassetfile

---
+++ Assets/Scripts/AddColision.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class AddColision : MonoBehaviour {
+
+	// Use this for initialization
+	public PlayerMovement movement; //A reference to the PlayerMovement Script
+    
+
+    //This function is called when we hit any obstacle
+    //We get information about collision amd called it as collosioninfo
+	void OnCollisionEnter(Collision collisioninfo)
+	{
+		if (collisioninfo.collider.tag == "Obstacle") 
+		{
+			
+			movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	//This is a reference to the Rigidbody component called  'rb'
+	public Rigidbody rb;
+	public float forwardForce=2000f;
+	public float sidewayForce=500f;
+	//use made the default Update method() to FixedUpadate to make unity to understand better of efficiency of Physics class
+	void FixedUpdate () 
+	{
+		rb.AddForce (00, 00, forwardForce*Time.deltaTime);	
+
+		if (Input.GetKey ("d")) 
+		{
+			rb.AddForce (sidewayForce * Time.deltaTime,00,00,ForceMode.VelocityChange);
+		}
+
+
+
+		if (Input.GetKey ("a")) 
+		{
+			rb.AddForce (-sidewayForce * Time.deltaTime,0,0, ForceMode.VelocityChange);
+		}
+        if(rb.position.y<11.1)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+	}
+}
